2ab51b26ee536f415d05b745a2f818ec627d37a5
mfrantz <mfrantz@redsealnetworks.com>
2015-02-20 10:35:37 -0800

    gremlin-v3: v0.0.22

--- gremlin-v3/gremlin-v3.d.ts
+++ gremlin-v3/gremlin-v3.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for gremlin-v3 0.0.21
+// Type definitions for gremlin-v3 0.0.22
 // Project: https://github.com/jimlloyd/gremlin-v3
 // Definitions by: Matt Frantz <https://github.com/mhfrantz/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
