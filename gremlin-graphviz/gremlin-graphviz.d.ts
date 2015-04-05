6e7ca5f3e0c9d4c986db16b45c8d791c9aac24e8
mfrantz <mfrantz@redsealnetworks.com>
2015-04-04 17:13:45 -0700

    gremlin-graphviz: v1.1.0 (use ts-tinkerpop instead of gremlin-v3)

--- gremlin-graphviz/gremlin-graphviz.d.ts
+++ gremlin-graphviz/gremlin-graphviz.d.ts
@@ -1,24 +1,24 @@
-// Type definitions for Gremlin-Graphviz 1.0.0
+// Type definitions for Gremlin-Graphviz 1.1.0
 // Project: https://github.com/mhfrantz/gremlin-graphviz
 // Definitions by: Matt Frantz <https://github.com/mhfrantz/>
 // Definitions: https://github.com/borisyankov/DefinitelyTyped
 
-/// <reference path="../gremlin-v3/gremlin-v3.d.ts"/>
+/// <reference path="../bluebird/bluebird.d.ts"/>
 /// <reference path="../graphviz/graphviz.d.ts"/>
-/// <reference path="../q/Q.d.ts"/>
+/// <reference path="../ts-tinkerpop/ts-tinkerpop.d.ts"/>
 
 declare module 'gremlin-graphviz' {
 
+  import BluePromise = require('bluebird');
   import graphviz = require('graphviz');
-  import Gremlin = require('gremlin-v3');
-  import Q = require('q');
+  import TP = require('ts-tinkerpop');
 
-  function makeGraph(gremlinGraph: makeGraph.GremlinGraph, opts?: any): Q.Promise<makeGraph.GraphvizGraph>;
+  function makeGraph(gremlinGraph: makeGraph.GremlinGraph, opts?: any): BluePromise<makeGraph.GraphvizGraph>;
 
   module makeGraph {
 
-    interface GremlinGraph extends Gremlin.GraphWrapper {}
-    interface GraphvizGraph extends graphviz.Graph {}
+    type GremlinGraph = Java.Graph;
+    type GraphvizGraph = graphviz.Graph;
 
   }
 
